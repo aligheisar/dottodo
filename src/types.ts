61797340f0b9864cd0bc7288db72bc05340d1176
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-03-02 13:28:14 +0330

    add some type for package file and add a addtodo command

--- src/types.ts
+++ src/types.ts
@@ -17,3 +17,53 @@ export interface VSCodeAPI {
   getState<T = any>(): T | undefined;
   setState<T = any>(newState: T): void;
 }
+
+// packagejson
+type activityBarItem = {
+  icon?: string;
+  id: string;
+  title: string;
+};
+
+interface viewsContainers {
+  activitybar?: activityBarItem[];
+}
+
+interface command {
+  category: string;
+  command: string;
+  enablement?: string;
+  icon?: string;
+  shortTitle?: string;
+  title: string;
+}
+
+type viewTypes = "webview" | "tree";
+type viewVisibility = "visible" | "hidden" | "collapsed";
+
+type viewItem = {
+  accessibilityHelpContent?: string;
+  icon?: string;
+  contextualTitle?: string;
+  id: string;
+  initialSize?: number;
+  name: string;
+  type: viewTypes;
+  visibility?: viewVisibility;
+  when?: string;
+};
+
+interface views {
+  [key: string]: viewItem[];
+}
+
+type contributes = {
+  commands?: command[];
+  viewsContainers: viewsContainers;
+  views: views;
+};
+
+export interface packageJson {
+  name: string;
+  contributes: contributes;
+}
