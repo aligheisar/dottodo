c5181e9e5a91d99338d379c080d610fcdf82c9fe
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-03-11 10:37:39 +0330

    make commands as const

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -3,11 +3,11 @@ import { EXTENSION_NAME } from "./general";
 export const COMMANDS = {
   INIT: `${EXTENSION_NAME}.init`,
   ADD_TODO: `${EXTENSION_NAME}.add-todo`,
-};
+} as const;
 
 export const POST_COMMANDS = {
   INIT: "init",
   LOADED: "loaded",
   UPDATE_TODO: "update-todo",
   UPDATE_SETTINGS: "update-settings",
-};
+} as const;
