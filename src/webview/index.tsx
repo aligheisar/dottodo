5c8307d24c692549dac51d6c3b6575443b288118
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-04-04 23:32:02 +0330

    update style.css to globals and move it to style folder

--- src/webview/index.tsx
+++ src/webview/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import Root from "./Root";
-import "./styles.css";
+import "./styles/globals.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<Root />);
