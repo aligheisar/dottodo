8ecc3609eec1b943c2e391ba2ad78afe7f5e6acb
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-06 08:58:14 +0330

    update commands

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -3,11 +3,17 @@ import { EXTENSION_NAME } from "./general";
 export const COMMANDS = {
   INIT: `${EXTENSION_NAME}.init`,
   ADD_TODO: `${EXTENSION_NAME}.add-todo`,
+  REMOVE_TODO: `${EXTENSION_NAME}.remove-todo`,
+  UPDATE_TODO: `${EXTENSION_NAME}.update-todo`,
+  UPDATE_TODO_LIST: `${EXTENSION_NAME}.update-todo-list`,
 } as const;
 
 export const POST_COMMANDS = {
   INIT: "init",
   LOADED: "loaded",
+  ADD_TODO: "add-todo",
+  REMOVE_TODO: "remove-todo",
   UPDATE_TODO: "update-todo",
+  UPDATE_TODO_LIST: "update-todo-list",
   UPDATE_SETTINGS: "update-settings",
 } as const;
