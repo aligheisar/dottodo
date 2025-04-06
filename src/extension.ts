62005d706d494820c3aa028baaaa423845acda51
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-06 10:52:50 +0330

    rename update todo to update todo list

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as vscode from "vscode";
-// import { nanoid } from "nanoid";
 import { TodoWebviewProvider } from "./todoWebview";
 import { TodoManager } from "./todoManager";
 import { COMMANDS } from "./constants/commands";
+import { Priority } from "./types";
 
 let TodoWebviewProviderInstance: TodoWebviewProvider;
 
@@ -10,33 +10,35 @@ export function activate(context: vscode.ExtensionContext) {
   const init = vscode.commands.registerCommand(COMMANDS.INIT, () => {
     TodoManager.initializeTodoFiles();
 
-    TodoWebviewProviderInstance.updateTodos();
+    TodoWebviewProviderInstance.updateTodoList();
   });
 
   const updateTodoList = vscode.commands.registerCommand(
     COMMANDS.UPDATE_TODO_LIST,
     () => {
-      TodoWebviewProviderInstance.updateTodos();
+      TodoWebviewProviderInstance.updateTodoList();
     }
   );
 
   const addTodo = vscode.commands.registerCommand(
     COMMANDS.ADD_TODO,
     async () => {
-      const result = await vscode.window.showInputBox({
+      const resultTask = await vscode.window.showInputBox({
         placeHolder: "Enter todo content",
         title: "Add Todo",
       });
 
-      if (result) {
-        // !!! use todo maker
-        TodoWebviewProviderInstance.addTodo({
-          created: new Date().toString(),
-          done: false,
-          id: "slkfjds",
-          priority: "medium",
-          task: "some task",
-        });
+      const priority = ["high", "medium", "low"];
+
+      const resultPriority = await vscode.window.showQuickPick(priority, {
+        title: "choose a priority",
+      });
+
+      if (resultTask) {
+        TodoWebviewProviderInstance.addTodo(
+          resultTask,
+          (resultPriority as Priority) || "medium"
+        );
       }
     }
   );
