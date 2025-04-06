517153ae7ded0e7fc15dae162aef71abe488d837
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-06 08:49:17 +0330

    add new error message

--- src/messages.ts
+++ src/messages.ts
@@ -1,3 +1,7 @@
-export const ERRORS = { NoFolderOpen: "No workspace folder open." };
+export const ERRORS = {
+  NoFolderOpen: "No workspace folder open.",
+  NotInit: "Extension not Init yet please init it",
+  SomethingHappens: "Something Happens operation is failed",
+};
 
 export const SUCCESS = { init: "ToDo files initialized in .todo folder!" };
