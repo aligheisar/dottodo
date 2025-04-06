7c7685146b893ce0febc1fd4087f2d4d9a5156f7
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-06 09:00:14 +0330

    update todo context

--- src/webview/context/TodosContext.tsx
+++ src/webview/context/TodosContext.tsx
@@ -6,42 +6,48 @@ import {
   useEffect,
 } from "react";
 import { Todo } from "../../types";
-import { getVSCodeApi } from "../VsCodeApi";
 import { POST_COMMANDS } from "../../constants/commands";
 import { useMessage } from "../hooks/use-message";
+import { loadedMessage, updateTodosListMessage } from "../utils/postMessages";
 
 export type ContextValues = {
   todos: Todo[] | null;
   loading: boolean;
+  refreshTodos: () => void;
 };
 
-const TodosContext = createContext<ContextValues>({
-  todos: null,
-  loading: true,
-});
+const TodosContext = createContext<ContextValues | null>(null);
 
-export const GetTodos = () => useContext(TodosContext);
+export const GetTodos = () => {
+  const context = useContext(TodosContext);
+
+  if (!context) throw new Error("you should use context in it provider");
+
+  return context;
+};
 
 const TodosProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState(true);
   const [todos, setTodos] = useState<Todo[] | null>(null);
 
-  const vscode = getVSCodeApi();
-
   useMessage({
-    [POST_COMMANDS.UPDATE_TODO](data) {
+    [POST_COMMANDS.UPDATE_TODO_LIST](data) {
       setTodos(data);
       setLoading(false);
     },
   });
 
   useEffect(() => {
-    vscode.postMessage({
-      command: POST_COMMANDS.LOADED,
-    });
+    loadedMessage();
   }, []);
 
-  const contextValues: ContextValues = { todos: todos, loading };
+  const refreshTodos = () => {
+    setLoading(true);
+
+    updateTodosListMessage();
+  };
+
+  const contextValues: ContextValues = { todos: todos, loading, refreshTodos };
   return (
     <TodosContext.Provider value={contextValues}>
       {children}
