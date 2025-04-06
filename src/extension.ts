6ba9dacf95075cb6ac2f9a6199c2f2db79fa7490
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-06 09:03:26 +0330

    update extension file to register new command and make a instance of todowebview to call new methods

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,56 @@
 import * as vscode from "vscode";
+// import { nanoid } from "nanoid";
 import { TodoWebviewProvider } from "./todoWebview";
 import { TodoManager } from "./todoManager";
 import { COMMANDS } from "./constants/commands";
 
+let TodoWebviewProviderInstance: TodoWebviewProvider;
+
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand(COMMANDS.INIT, () => {
+  const init = vscode.commands.registerCommand(COMMANDS.INIT, () => {
     TodoManager.initializeTodoFiles();
+
+    TodoWebviewProviderInstance.updateTodos();
   });
+
+  const updateTodoList = vscode.commands.registerCommand(
+    COMMANDS.UPDATE_TODO_LIST,
+    () => {
+      TodoWebviewProviderInstance.updateTodos();
+    }
+  );
+
+  const addTodo = vscode.commands.registerCommand(
+    COMMANDS.ADD_TODO,
+    async () => {
+      const result = await vscode.window.showInputBox({
+        placeHolder: "Enter todo content",
+        title: "Add Todo",
+      });
+
+      if (result) {
+        // !!! use todo maker
+        TodoWebviewProviderInstance.addTodo({
+          created: new Date().toString(),
+          done: false,
+          id: "slkfjds",
+          priority: "medium",
+          task: "some task",
+        });
+      }
+    }
+  );
+
+  TodoWebviewProviderInstance = new TodoWebviewProvider(context.extensionUri);
 
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
       TodoWebviewProvider.viewType,
-      new TodoWebviewProvider(context.extensionUri)
+      TodoWebviewProviderInstance
     )
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(init, updateTodoList, addTodo);
 }
 
 export function deactivate() {}
