1506141b89fdb4372c6e107ec31d2132632a3288
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-18 15:38:06 +0330

    add a util format data

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,3 @@
+export const formatDate = (date: Date): string => {
+  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
