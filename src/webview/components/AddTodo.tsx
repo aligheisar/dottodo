bc0ab9f1ce885925178e90a6667832276b7de4e6
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-18 15:39:52 +0330

    modify add todo comp

--- src/webview/components/AddTodo.tsx
+++ src/webview/components/AddTodo.tsx
@@ -1,20 +1,32 @@
 import { FormEvent, useRef } from "react";
+import { Priority } from "../../types";
+import { addTodoMessage } from "../utils/postMessages";
 
 const AddTodo = () => {
-  const input = useRef<HTMLInputElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const selectRef = useRef<HTMLSelectElement | null>(null);
 
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!input.current) return;
+    if (!inputRef.current || !selectRef.current) return;
 
-    input.current.value = "";
-    input.current.focus();
+    addTodoMessage(inputRef.current.value, selectRef.current.value as Priority);
+
+    selectRef.current.value = "medium";
+
+    inputRef.current.value = "";
+    inputRef.current.focus();
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <input autoFocus ref={input} type="text" placeholder="Enter Todo" />
+    <form className="inline-padding" onSubmit={handleFormSubmit}>
+      <input autoFocus ref={inputRef} type="text" placeholder="Enter Todo" />
+      <select ref={selectRef} defaultValue="medium">
+        <option value="high">High</option>
+        <option value="medium">Medium</option>
+        <option value="low">Low</option>
+      </select>
       <button type="submit">
         Add <span className="codicon codicon-add"></span>
       </button>
