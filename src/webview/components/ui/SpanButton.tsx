b5e92d79f871fd33b292e56036e25f3e40f13e9f
Ali Gheisar <a.gheisar.sh@gmail.com>
2025-04-18 15:38:53 +0330

    add todo component

--- src/webview/components/ui/SpanButton.tsx
+++ src/webview/components/ui/SpanButton.tsx
@@ -1,19 +1,29 @@
-import React, { FC, HTMLAttributes } from "react";
+import React, { FC, HTMLAttributes, KeyboardEvent, MouseEvent } from "react";
 
 type Props = HTMLAttributes<HTMLSpanElement> & {
   codiconName: string;
   className?: string;
   tabIndex?: number;
+  func?: (
+    e: MouseEvent<HTMLSpanElement> | KeyboardEvent<HTMLSpanElement>
+  ) => void;
 };
 
 const SpanButton: FC<Props> = ({
   codiconName,
   className,
   tabIndex = 0,
+  func,
   ...props
 }) => {
   return (
     <span
+      onClick={(e) => {
+        if (func) func(e);
+      }}
+      onKeyUp={(e) => {
+        if (e.keyCode === 13 && func) func(e);
+      }}
       tabIndex={tabIndex}
       className={`span-button codicon codicon-${codiconName} ${className}`}
       {...props}
