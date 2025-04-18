581f6dfea8cb0ade12d91b1d8e6a99427ed6b4f5
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-04-18 15:38:35 +0330

    add loading component

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -2,11 +2,12 @@ import { GetTodos } from "./context/TodosContext";
 import AddTodo from "./components/AddTodo";
 import TodoContainer from "./components/TodoContainer";
 import NotInit from "./components/NotInit";
+import Loading from "./components/Loading";
 
 const App = () => {
   const { loading, todos } = GetTodos();
 
-  if (loading && !todos) return <p>loading...</p>;
+  if (loading && !todos) return <Loading />;
 
   return todos ? (
     <>
---
+++ src/webview/components/Loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="center-child font-base">
+      <span>Loading...</span>
+    </div>
+  );
+};
+
+export default Loading;
